854d6ebcd391a744a43be971391b5f260dfafe14
Denis Lomakin <dkartik30@gmail.com>
2024-10-05 07:32:48 +0300

    Fixed SearchByName issue

--- src/main/java/com/example/movie/controller/ActorController.java
+++ src/main/java/com/example/movie/controller/ActorController.java
@@ -7,11 +7,13 @@ import com.example.movie.entity.Actor;
 import com.example.movie.service.ActorService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.*;
 import jakarta.validation.Valid;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/actors")
@@ -32,12 +34,27 @@ public class ActorController {
 
     @GetMapping
     public List<ActorResponseDto> getAllActors(
-            @RequestParam(required = false)Integer page,
-            @RequestParam(required = false)Integer size)
+            @RequestParam(required = false) Integer page,
+            @RequestParam(required = false) Integer size,
+            @RequestParam(required = false) String name)
     {
+        if (name != null && !name.isEmpty()) {
+            Pageable pageable = PageRequest.of(page != null ? page : 0, size != null ? size : 10);
+            return actorService.searchActorsByName(name, pageable)
+                    .stream()
+                    .map(this::convertToDto)
+                    .collect(Collectors.toList());
+        } else {
             return actorService.getAllActors(page, size);
+        }
     }
-
+    private ActorResponseDto convertToDto(Actor actor) {
+        return ActorResponseDto.builder()
+                .id(actor.getId())
+                .name(actor.getName())
+                .birthDate(actor.getBirthDate())
+                .build();
+    }
     @PatchMapping("/{id}")
     public ActorResponseDto updateActor(@PathVariable Long id, @RequestBody ActorRequestDto actorDetails) {
         return actorService.updateActor(id, actorDetails);
